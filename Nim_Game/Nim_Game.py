3af39bb2dd3d5b9f5c3d61a0af64f41a795a8ad2
lijingchn <lijingjing.chn@gmail.com>
2016-07-06 08:42:22 +0800

    Nim Game

---
+++ Nim_Game/Nim_Game.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Solution(object):
+    def canWinNim(self,n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n%4:
+            return True
+        else:
+            return False
